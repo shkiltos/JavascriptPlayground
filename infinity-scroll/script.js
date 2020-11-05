5d0a0f18756989ae33bd6bfffa92365a687052e0
Anton Shkilevich <toha.srednii@gmail.com>
2020-11-05 13:53:54 +0300

    Infinite scroll

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,12 +1,23 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 const count = 10;
 const apiKey = 'WnrhKTNg7CybSv1saCBHbTAlML7EA_9EC49AEwVaW1U';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 function setAttributes(element, attributes) {
     for (const key in attributes) {
@@ -16,6 +27,8 @@ function setAttributes(element, attributes) {
 
 // Create elements for photos, add to DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         // Create <a> to link unsplash
         const item = document.createElement('a');
@@ -30,6 +43,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        // Event listener that checks when each is finished loading
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then both inside image-container
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -46,6 +61,14 @@ async function getPhotos() {
         console.log(error);
     }
 }
+
+// Check if the scroll is near the bottom of the page, load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // On load
 getPhotos();
